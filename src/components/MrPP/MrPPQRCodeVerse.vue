6f9476a507a4be0edfc60b6d3304a26756507b63
dirui <dirui@bujiaban.com>
2022-09-04 13:51:24 +0800

    增加local 本地信息

--- src/components/MrPP/MrPPQRCodeVerse.vue
+++ src/components/MrPP/MrPPQRCodeVerse.vue
@@ -1,24 +1,20 @@
 <template>
-
   <el-dialog
-
     :title="dialogTitle"
     :visible.sync="dialogVisible"
     width="80%"
-
     :close-on-click-modal="false"
   >
-    <el-card class="box-card" style="text-align:center">
+    <el-card class="box-card" style="text-align: center">
       <qrcode-vue :value="value" :size="size" level="H" />
-
     </el-card>
     <span slot="footer" class="dialog-footer">
-      <el-button @click="dialogVisible=false">取 消</el-button>
+      <el-button @click="dialogVisible = false">取 消</el-button>
     </span>
-
   </el-dialog>
 </template>
 <script>
+import environment from '@/environment.js'
 import 'element-ui/lib/theme-chalk/display.css'
 import QrcodeVue from 'qrcode.vue'
 export default {
@@ -35,12 +31,11 @@ export default {
     }
   },
   'document.body.clientwidth': {
-    message: function() {
-    }
+    message: function () {}
   },
   mounted() {
     const self = this
-    self.$nextTick(function() {
+    self.$nextTick(function () {
       this.$on('open', self.open)
     })
     self.onresize()
@@ -50,17 +45,17 @@ export default {
     window.removeEventListener('resize', self.onresize)
   },
   methods: {
-
     onresize() {
       this.size = Math.min(500, window.innerWidth * 0.8 - 80)
     },
     open(id) {
       const self = this
-      const data = { projectId: id, 'veri': 'MrPP.com' }
+      const data = environment.local
+        ? { projectId: id, api: environment.api, veri: 'MrPP.com' }
+        : { projectId: id, veri: 'MrPP.com' }
       self.value = JSON.stringify(data)
       self.dialogVisible = true
     }
   }
 }
 </script>
-
