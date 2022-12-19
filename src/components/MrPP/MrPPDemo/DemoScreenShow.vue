e4b823265025fc4871c7c816aa2813184db2bd00
delancy <34211046@qq.com>
2022-12-19 15:22:20 +0800

    <优化> 响应式设备匹配基础优化

--- src/components/MrPP/MrPPDemo/DemoScreenShow.vue
+++ src/components/MrPP/MrPPDemo/DemoScreenShow.vue
@@ -2,8 +2,9 @@
   <!-- 模型展示-->
   <section>
     <div class="modle_box">
-      <!-- v-drag指令 -->
+      <!-- 外框架元素 -->
       <div v-show="isShow" class="modle_init">
+        <!-- 3D渲染元素 -->
         <div id="container" />
       </div>
     </div>
@@ -13,6 +14,7 @@
 <script>
 import * as Three from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import ElementResizeDetector from 'element-resize-detector'
 export default {
   name: 'DemoThreeScreen',
 
@@ -37,6 +39,8 @@ export default {
       this.isShow = !this.isShow
     },
     init() {
+      const self = this
+
       const container = document.getElementById('container')
 
       this.camera = new Three.PerspectiveCamera(
@@ -69,7 +73,18 @@ export default {
 
       container.appendChild(this.renderer.domElement)
 
-      this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+      const erd = new ElementResizeDetector()
+      erd.listenTo(container, () => {
+        if (!self.sleep) {
+          const width = container.clientWidth
+          const height = container.clientHeight
+          // self.renderer.setViewport(0, 0, width, height)
+          self.renderer.setSize(width, height)
+          self.camera.aspect = width / height
+          self.camera.updateProjectionMatrix()
+        }
+      })
+      // this.controls = new OrbitControls(this.camera, this.renderer.domElement)
     },
 
     animate() {
@@ -86,21 +101,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.modle_box {
-  .modle_init {
-    position: absolute;
-    top: 0;
-    right: 5%;
-    // text-align: center;
-    width: 50%;
-    height: calc(30vw + 60px);
-    // 半屏效果
-    // width: 50%;
-    // height: calc(30vw + 60px);
-
-    // line-height: 50px;
-    z-index: 50;
-  }
+.modle_init {
+  max-width: 800px;
+  max-height: 650px;
+  position: absolute;
+  top: 50px;
+  left: calc(48% + 30px);
+  width: 50%;
+  height: 30vw;
+  // 半屏效果
+  // width: 50%;
+  // height: calc(30vw + 60px);
+  // line-height: 50px;
+  z-index: 50;
 }
 #container {
   height: 100%;
