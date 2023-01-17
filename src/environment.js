c8cf49eba1ce014e7a4d29dacf17603676248dbf
dirui <dirui@bujiaban.com>
2023-01-17 22:39:13 +0800

    no message

--- src/environment.js
+++ src/environment.js
@@ -1,15 +1,15 @@
 function canRegister() {
   return 'mrpp.com' === process.env.VUE_APP_BASE_MODE
 }
+
+function canWeb() {
+  return 'mrpp.com' === process.env.VUE_APP_BASE_MODE
+}
 function canSetup() {
   return (
     'local' === process.env.VUE_APP_BASE_MODE ||
     '4mr.cn' === process.env.VUE_APP_BASE_MODE
   )
-}
-
-function canWeb() {
-  return 'mrpp.com' === process.env.VUE_APP_BASE_MODE
 }
 
 function subtitle() {
