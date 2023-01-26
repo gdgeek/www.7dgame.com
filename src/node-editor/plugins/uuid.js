2e1b608e885fc4d431332d3266585066c0d5d386
dirui <dirui@bujiaban.com>
2023-01-26 13:38:48 +0800

    解决了uuid在clone中重复的问题，（verse禁止clone，meta在里面发现重复uuid时候重新分配）

---
+++ src/node-editor/plugins/uuid.js
@@ -0,0 +1,31 @@
+/*!
+ * rete-vue-render-plugin v0.5.1
+ * (c) 2021 Vitaliy Stoliarov
+ * Released under the MIT license.
+ */
+import { v4 as uuidv4 } from 'uuid'
+const set = new Set()
+function install(editor, options) {
+  editor.on('nodecreate', component => {
+    options.forEach((value, index, arr) => {
+      if (value.component === component.name) {
+        const control = component.controls.get(value.target)
+        if (control !== null) {
+          if (
+            typeof component.data[value.target] === 'undefined' ||
+            set.has(component.data[value.target])
+          ) {
+            component.data[value.target] = uuidv4()
+          }
+          set.add(component.data[value.target])
+        }
+      }
+    })
+
+    return true
+  })
+}
+export default {
+  name: 'rete-uuid',
+  install: install
+}
