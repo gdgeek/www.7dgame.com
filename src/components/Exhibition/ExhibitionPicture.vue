5ba9db8ba186643b0942a0e76e6e7ed06c7eea32
delancy <34211046@qq.com>
2023-02-10 18:24:55 +0800

    <优化>  调整一处参数

--- src/components/Exhibition/ExhibitionPicture.vue
+++ src/components/Exhibition/ExhibitionPicture.vue
@@ -3,12 +3,12 @@
   <section>
     <el-row :gutter="40" style="padding: 5% 10%">
       <el-col :span="24">
-        <div align="center" style="padding: 30px 5%">
-          <div class="exhibition-title-h3 p-title-m">亲自体验元宇宙展馆</div>
-          <p class="exhibition-content-small p-title-ss">
+        <div align="center" style="padding: 30px 5% 60px">
+          <h1 class="p-title-m">亲自体验元宇宙展馆</h1>
+          <h4 class="title-box">
             世界的维度正在发生变化。我们引领场馆的的数字可视化，利用元宇宙数字孪生，快速搭建场景
             实现任何类型的空间数字化——展馆、办公室和酒店及商场。用技术赋能空间交互，实现前所未有的价值。
-          </p>
+          </h4>
         </div>
       </el-col>
       <!-- picture部分 -->
@@ -19,7 +19,7 @@
             style="width: 100%; border-radius: 5px"
           />
         </div>
-        <div class="img-title p-title-s">满足不同的场所</div>
+        <h2 class="p-title-s">满足不同的场所</h2>
       </el-col>
       <br />
       <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8" align="center">
@@ -29,7 +29,7 @@
             style="width: 100%; border-radius: 5px"
           />
         </div>
-        <div class="img-title p-title-s">场景扫描定位</div>
+        <h2 class="p-title-s">场景扫描定位</h2>
       </el-col>
       <br />
       <!-- 视频位置 -->
@@ -40,13 +40,14 @@
             style="width: 100%; border-radius: 5px"
           />
         </div>
-        <div class="img-title p-title-s">个性化展馆布置</div>
+        <h2 class="p-title-s">个性化展馆布置</h2>
       </el-col>
     </el-row>
   </section>
 </template>
 
 <script>
+import '@/assets/font/font.css'
 export default {
   name: 'ExhibitionPicture',
   data() {
@@ -70,12 +71,6 @@ export default {
   font-size: 22px;
   color: #222222;
   font-weight: 400;
-}
-.exhibition-title-h3 {
-  padding: 20px;
-  font-size: 36px;
-  font-weight: 400;
-  color: #222222;
 }
 
 .exhibition-content-small {
