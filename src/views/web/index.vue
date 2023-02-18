aa3f823dede8a3503d1450310e0c82e7a40d6b33
dirui <dirui@bujiaban.com>
2023-02-18 10:48:30 +0800

    no message

--- src/views/web/index.vue
+++ src/views/web/index.vue
@@ -51,12 +51,12 @@ export default {
           image: '/media/bg/business1.jpg'
         },
         {
-          title: '2定制开发demo，5000元直接赠送',
+          title: '定制开发demo，5000元直接赠送',
           text: '我们将为您选择的案例提供Demo和报价。为下一步开发打好基础。如果您对我们的demo满意，当我们达成合作进行开发，5000可抵扣工程款。',
           image: '/media/bg/business2.jpg'
         },
         {
-          title: '3开发完成后的技术支持',
+          title: '开发完成后的技术支持',
           text: '当开发完成验收后，您有任何疑问，我们都会有专家及时帮助您，如果您有需要，我们将会派出专业人员实地提供技术指导。',
           image: '/media/bg/business3.jpg'
         }
